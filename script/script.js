8535411472637dd4134a798e42b730a97aaa6526
unknown <arinconu@ibero.edu.co>
2021-05-20 19:53:58 -0500

    para hacer comentarios

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function confirmacion(){
+    return confirm("Deseas ir al Facebook de Owner");
+}
+function confirmacion2(){
+    return confirm("Deseas ir al Instagram de Owner");
+}
+function confirmacion3(){
+    return confirm("Deseas enviar los datos");
+}
